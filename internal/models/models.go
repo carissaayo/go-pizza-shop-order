ed134f24202f0f0c7ad1d9b35c5df9f3e169af46
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-03 10:49:29 +0100

    user model

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,6 +9,7 @@ import (
 
 type DBModel struct {
 	Order OrderModel
+	User  UserModel
 }
 
 func InitDB(dataSourceName string) (*DBModel, error) {
@@ -18,7 +19,7 @@ func InitDB(dataSourceName string) (*DBModel, error) {
 		return nil, fmt.Errorf("Failed to migrate database: %v", err)
 	}
 
-	err = db.AutoMigrate(&Order{}, &OrderItem{})
+	err = db.AutoMigrate(&Order{}, &OrderItem{}, &User{})
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to migrate database: %v", err)
@@ -27,6 +28,7 @@ func InitDB(dataSourceName string) (*DBModel, error) {
 
 	dbModel := &DBModel{
 		Order: OrderModel{DB: db},
+		User:  UserModel{DB: db},
 	}
 
 	return dbModel, nil
