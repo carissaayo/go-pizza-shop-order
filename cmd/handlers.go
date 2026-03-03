03a1f3295212f40f19ee0b56df9bd49b75f0e20e
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-03 14:30:20 +0100

    more

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -4,10 +4,12 @@ import "github.com/carissaayo/go-pizza-shop-order/internal/models"
 
 type Handler struct {
 	orders *models.OrderModel
+	users  *models.UserModel
 }
 
 func NewHandler(dbModel *models.DBModel) *Handler {
 	return &Handler{
 		orders: &dbModel.Order,
+		users:  &dbModel.User,
 	}
 }
