db330bb2ddb2829404e9573339052b64ce76570a
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-03 17:06:01 +0100

    more and more

--- cmd/routes.go
+++ cmd/routes.go
@@ -20,6 +20,8 @@ func setupRoutes(router *gin.Engine, h *Handler, store sessions.Store) {
 	admin.Use(h.AuthMiddleware())
 	{
 		admin.GET("", h.ServeAdminDashboard)
+		admin.POST("/order/:id/update", h.HandleOrderPut)
+		admin.POST("/order/:id/delete", h.HandleOrderDelete)
 	}
 
 	router.Static("/static", "./templates/static")
