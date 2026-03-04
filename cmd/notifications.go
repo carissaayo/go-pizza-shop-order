d8ada71dce629970da6101a289c83958f6ce0ba7
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-04 10:58:28 +0100

    notify

--- cmd/notifications.go
+++ cmd/notifications.go
@@ -1,18 +1,52 @@
 package main
 
-type NotificationManager struct{
+import "sync"
+
+type NotificationManager struct {
 	clients map[string]map[chan string]bool
+	mu      sync.RWMutex
 }
 
-clients map[string]map[chan string]bool={
-	"order:123":{
-		0xc0000a4000: true,
-		0xc0000a4060:true,
-	},
-	"order:456":{
-		0xc0000a40c0:true,
-	},
-	"admin:new_orders":{
-		0xc0000a4180:true,
-	},
+func NewNotificationManager() *NotificationManager {
+	return &NotificationManager{
+		clients: make(map[string]map[chan string]bool),
+	}
+}
+
+func (n *NotificationManager) AddClient(key string, client chan string) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if n.clients[key] == nil {
+		n.clients[key] = make(map[chan string]bool)
+	}
+
+	n.clients[key][client] = true
+}
+
+func (n *NotificationManager) RemoveClient(key string, client chan string) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if clients := n.clients[key]; clients != nil {
+		delete(clients, client)
+
+		if len(clients) == 0 {
+			delete(n.clients, key)
+		}
+	}
+
+	close(client)
+}
+
+func (n *NotificationManager) Notify(key, message string) {
+	n.mu.RLock()
+	defer n.mu.Unlock()
+
+	for client := range n.clients[key] {
+		select {
+		case client <- message:
+		default:
+		}
+	}
 }
